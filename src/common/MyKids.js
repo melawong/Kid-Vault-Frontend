fd389c5228bfa0426622082e1114d72d9aa7bb2b
Melanie Wong <wong.mela@gmail.com>
2022-07-06 17:58:46 -0700

    got axios request working, list of kids show on mykids page

--- src/common/MyKids.js
+++ src/common/MyKids.js
@@ -1,33 +1,32 @@
 import MomApi from "../helpers/momApi";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function MyKids() {
+  const [kids, setKids] = useState([]);
 
-  // async function getMyKids() {
-  //   const myKids = await MomApi.getMyKids();
-  //   console.log("MY KIDS", myKids);
-  //   return (
-  //     myKids.map(kid => <li> {kid.first_name} {kid.last_name} </li>)
-  //   );
+  useEffect(function getKidsOnMount() {
+    async function getKids() {
 
-  // }
+      const response = await axios({
+        url: process.env.REACT_APP_BASE_URL,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: process.env.REACT_APP_API_KEY,
+        },
+        data: { query: '{myQuery {first_name last_name}}' },
+      }).then(result => result.data.data);
 
-  const myKids = fetch(process.env.REACT_APP_BASE_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: process.env.REACT_APP_API_KEY,
-    },
-    body: JSON.stringify({ query: '{myQuery {first_name last_name}}' }),
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Success:', data);
-    });
+      setKids(response.myQuery);
+    }
+    getKids();
+  }, []);
 
 
   return (<div>
     <ul>
-
+      {kids.map(kid => <li> {`${kid.first_name} ${kid.last_name}`}</li>)}
     </ul>
   </div>);
 
