87f9eaa5fa110b3a6f5c921957b25e64bd33ffcb
Melanie Wong <wong.mela@gmail.com>
2022-07-10 12:22:07 -0700

    put the accordion in the second column instead of underneath

--- src/common/KidDetail.js
+++ src/common/KidDetail.js
@@ -47,37 +47,37 @@ function KidDetail() {
               <h5 className="mb-3 fw-light text-start">Classroom: {kid.classroom}</h5>
               <p className="mb-3 fw-light text-start">Birthday: {kid.birth_date}</p>
               <p className="mb-3 fw-light text-start">Primary Guardian: {kid.contacts[0].name}</p>
-            </div>
-            <h3>{kid.first_name}'s Info: </h3>
-            <div className="accordion ms-5 me-5" id="infoAccordion">
-              <div className="accordion-item ms-5 me-5">
-                <h2 className="accordion-header" id="headingOne">
-                  <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    Contacts
-                  </button>
-                </h2>
-                <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                  <div className="accordion-body">
-                    <ul>
-                      {kid.contacts.map(contact => <li> <strong> {contact.name}</strong>: {contact.relation}, {contact.email}, {contact.phone} </li>)}
-                    </ul>
+              <h3 classname="text-start">{kid.first_name}'s Info: </h3>
+              <div className="accordion" id="infoAccordion">
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingOne">
+                    <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                      Contacts
+                    </button>
+                  </h2>
+                  <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div className="accordion-body">
+                      <ul>
+                        {kid.contacts.map(contact => <li> <strong> {contact.name}</strong>: {contact.relation}, {contact.email}, {contact.phone} </li>)}
+                      </ul>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="accordion-item ms-5 me-5">
-                <h2 className="accordion-header" id="headingTwo">
-                  <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                    Medical Record
-                  </button>
-                </h2>
-                <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                  <div className="accordion-body">
-                    <h5><strong>Basic Details</strong></h5>
-                    <p> Current Height: {kid.medical_record.student_height} in.</p>
-                    <p> Current Weight: {kid.medical_record.student_weight} lbs.</p>
-                    <h5><strong>Immunizations</strong></h5>
-                    <p>Covid 1st dose: {kid.medical_record.covid1}</p>
-                    <p>Covid 2nd dose: {kid.medical_record.covid2}</p>
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingTwo">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                      Medical Record
+                    </button>
+                  </h2>
+                  <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    <div className="accordion-body">
+                      <h5><strong>Basic Details</strong></h5>
+                      <p> Current Height: {kid.medical_record.student_height} in.</p>
+                      <p> Current Weight: {kid.medical_record.student_weight} lbs.</p>
+                      <h5><strong>Immunizations</strong></h5>
+                      <p>Covid 1st dose: {kid.medical_record.covid1}</p>
+                      <p>Covid 2nd dose: {kid.medical_record.covid2}</p>
+                    </div>
                   </div>
                 </div>
               </div>
