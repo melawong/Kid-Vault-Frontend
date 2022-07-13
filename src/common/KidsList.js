e517154da6b55293efbd7ffc2fdf76bf2003d43f
Anita Lee <anita.lee@gmail.com>
2022-07-13 18:51:07 -0400

    Center kid cards

--- src/common/KidsList.js
+++ src/common/KidsList.js
@@ -23,7 +23,7 @@ function KidsList() {
   }, []);
 
   return hasLoaded ? (
-    <div className="row display-flex flex-wrap">
+    <div className="row display-flex flex-wrap justify-content-center mt-3">
       {kids.map(kid =>
         <div className="col-2 ms-2 my-2">
           <KidCard key={kid.id} kid={kid} />
