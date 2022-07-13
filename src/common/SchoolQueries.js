260e1a23d11e0c21975987198dd7e18abf182874
Melanie Wong <wong.mela@gmail.com>
2022-07-13 10:35:53 -0700

    WIP schoolqueries page

--- src/common/SchoolQueries.js
+++ src/common/SchoolQueries.js
@@ -1,6 +1,16 @@
-function SchoolQueries (){
+function SchoolQueries() {
 
-  return <p>hi</p>
+  return (<div class="form-group row">
+    <label for="exampleSelect1" className="form-label mt-4">Choose A Query</label>
+    <select className="form-select w-50 mx-auto" id="exampleSelect1">
+      <option>Covid Positive Student Contact List</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+    <button className="btn btn-info w-25 mx-auto">Run</button>
+  </div>);
 }
 
-export default SchoolQueries
+export default SchoolQueries;
