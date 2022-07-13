7558131f277794061276468ca3b85953b52b39f0
Melanie Wong <wong.mela@gmail.com>
2022-07-13 10:48:09 -0700

    WIP schoolqueries page

--- src/common/SchoolQueries.js
+++ src/common/SchoolQueries.js
@@ -1,16 +1,46 @@
-function SchoolQueries() {
+import { useState, useContext } from "react";
+import UserContext from "../userContext";
 
-  return (<div class="form-group row">
-    <label for="exampleSelect1" className="form-label mt-4">Choose A Query</label>
-    <select className="form-select w-50 mx-auto" id="exampleSelect1">
-      <option>Covid Positive Student Contact List</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </select>
-    <button className="btn btn-info w-25 mx-auto">Run</button>
-  </div>);
+function SchoolQueries() {
+  const { handleSchoolQuery } = useContext(UserContext);
+  const [hasSubmitted, setHasSubmitted] = useState(false);
+  const [formData, setFormData] = useState({ chosenQuery: "Covid Positive Student Contact List" });
+
+  /** Update form input. */
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  }
+
+  /** Call parent function and clear form. */
+  async function handleSubmit(evt) {
+    evt.preventDefault();
+    await handleSchoolQuery(formData);
+    setFormData(null);
+    setHasSubmitted(true);
+  }
+
+  return (
+    <>
+      <form class="SchoolQueriesForm form-group row" onSubmit={handleSubmit}>
+        <label htmlFor="chosenQuery" className="form-label mt-4">Choose A Query</label>
+        <select
+          onChange={handleChange}
+          className="form-select w-50 mx-auto"
+          id="chosenQuery"
+          value={formData.chosenQuery}
+          name="chosenQuery"
+        >
+          <option>Covid Positive Student Contact List</option>
+          <option>2</option>
+          <option>3</option>
+        </select>
+        <button className="btn btn-info w-25 mx-auto">Run</button>
+      </form>
+      <div>
+
+      </div>
+    </>);
 }
 
 export default SchoolQueries;
