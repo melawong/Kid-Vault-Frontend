5a0926f004c43413baa1d4cc8ff307ee32ed13a8
Melanie Wong <wong.mela@gmail.com>
2022-07-12 22:55:08 -0700

    made kid list smaller squares and updated kid card

--- src/common/KidCard.js
+++ src/common/KidCard.js
@@ -9,30 +9,20 @@ import { Link } from "react-router-dom";
  */
 
 function KidCard({ kid }) {
-
-  kid.imgUrl = "https://www.parsonsphotography.com/assets/img_pages/fall_sample1-86c43874e4eb031c820526b6ba0eafb304b869d1acf9d82981a09fcef7405edc.jpg";
-  kid.fullName = `${kid.first_name}`;
-  kid.image_url = `${kid.image_url}`;
-
   return (
     <Link
       to={`/mykids/${kid.id}`}
-      style={{ textDecoration: "none", maxWidth: "50rem" }}
-      className="card text-dark bg-white mb-3 mx-5 d-flex"
+      className="card text-dark bg-white  h-100 w-100"
     >
-      <div class="container">
-        <div class="row">
-          <div class="col-sm" className="card-body align-center">
-            <p>
-              <img className="img-thumbnail" src={kid.image_url} alt={kid.fullName}/>
-            </p>
-            <h4 className="card-title">
-              {kid.fullName}
-            </h4>
-          </div>
-        </div>
+      <div className="card-body align-center">
+        <p>
+          <img className="img-thumbnail h-75 w-75" src={kid.image_url} alt={`${kid.first_name} ${kid.last_name}`} />
+        </p>
+        <h4 className="card-title">
+          {kid.first_name} {kid.last_name}
+        </h4>
       </div>
-      
+
     </Link>
   );
 }
--- src/common/KidsList.js
+++ src/common/KidsList.js
@@ -16,12 +16,14 @@ function KidsList() {
 
   if (kids.length) {
     return (
-      <div>
-        {kids.map(kid => <KidCard key={kid.id} kid={kid} />)}
+      <div className="row">
+        {kids.map(kid => <div className="col-2 ms-2 my-2">
+          <KidCard key={kid.id} kid={kid} />
+        </div>)}
       </div>);
   }
 
-  return <p> Loading... </p>
+  return <p> Loading... </p>;
 
 }
 
