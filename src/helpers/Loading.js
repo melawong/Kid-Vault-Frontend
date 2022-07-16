014263d7c259b425c6131e070a2aa058992603a6
Melanie Wong <wong.mela@gmail.com>
2022-07-16 15:11:23 -0700

    small changes

--- src/helpers/Loading.js
+++ src/helpers/Loading.js
@@ -6,7 +6,7 @@ function Loading() {
     <div className="row">
       <div className="col-6 mx-auto my-5">
         <ReactLoading className="mt-5 mx-auto" type={"spinningBubbles"} color={"#fffff"} height={'30%'} width={'20%'} />
-        <p><strong className="mt-1 my-5">Loading...</strong></p>
+        <p><strong className="mt-5 my-5">Loading...</strong></p>
         <p className="mt-5">If you're stuck, please refresh and try again! {":)"}</p>
       </div>
     </div>
